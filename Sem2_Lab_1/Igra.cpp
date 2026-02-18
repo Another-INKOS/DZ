3361951b0f5eb3cfa04fc2ad46d5b3a99d5c376e
Another-INKOS <egorov082007@gmail.com>
2026-02-18 20:23:25 +0700

    Create Igra.cpp

---
+++ Sem2_Lab_1/Igra.cpp
@@ -0,0 +1,28 @@
+#include "Igra.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void Igra::print() const {
+	cout << name << " " << color << " " << material << " " << size << " " << rating << " "<<endl;
+}
+void sortByColor(Igra arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j].color > arr[j + 1].color) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void sortByEarsize(Igra arr[], int size) {
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j].rating > arr[j + 1].rating) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
