b7191acc0b03f88e8d28469e97f3fcfb8693a57c
Another-INKOS <egorov082007@gmail.com>
2026-05-17 13:44:42 +0700

    Create Rational.h

---
+++ Sem2_Lab4/Task2/Rational.h
@@ -0,0 +1,39 @@
+#ifndef RATIONAL_H
+#define RATIONAL_H
+
+#include <iostream>
+
+class Rational {
+public:
+    // Методы для получения числителя и знаменателя
+    int Numerator() const;
+    int Denominator() const;
+    Rational operator+() const;
+    Rational operator-() const;
+
+    Rational& operator+=(const Rational& other);
+    Rational& operator-=(const Rational& other);
+    Rational& operator*=(const Rational& other);
+    Rational& operator/=(const Rational& other);
+
+    // Вспомогательная функция для вывода в поток
+    friend std::ostream& operator<<(std::ostream& os, const Rational& r);
+
+private:
+    int num; // числитель
+    int den; // знаменатель
+
+    void simplify();
+};
+
+// Бинарные арифметические операторы
+Rational operator+(Rational lhs, const Rational& rhs);
+Rational operator-(Rational lhs, const Rational& rhs);
+Rational operator*(Rational lhs, const Rational& rhs);
+Rational operator/(Rational lhs, const Rational& rhs);
+
+// Операторы сравнения
+bool operator==(const Rational& lhs, const Rational& rhs);
+bool operator!=(const Rational& lhs, const Rational& rhs);
+
+#endif
